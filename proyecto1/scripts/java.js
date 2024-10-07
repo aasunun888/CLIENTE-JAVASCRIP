6b672ac31da5faafae5cb608758e73123e3cfe73
APF92 <alperezf92@gmail.com>
2024-10-07 20:55:42 +0200

    proyecto1

---
+++ proyecto1/scripts/java.js
@@ -0,0 +1,30 @@
+/*let letString = '';
+var varInt = 0;
+
+
+    function a (){
+        console.log("soy la funcion A");
+        c();
+        }
+        
+       
+        setTimeout( function b (){
+            console.log("soy la funcion b");
+        },0);
+
+        function c (){
+            console.log("soy la funcion C");
+        }
+
+
+
+a();
+console.log(typeof varInt);*/
+
+function diAlgo(){
+    alert("hola");
+
+}
+
+
+
